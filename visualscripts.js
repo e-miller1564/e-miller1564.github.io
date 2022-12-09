7747cae65fa63dbc5ff0fa991fd71d5e4efd906f
Erik Miller <erik.miller1564@gmail.com>
2022-12-09 13:33:36 -0500

    Created Webpage

--- visualscripts.js
+++ visualscripts.js
@@ -2,9 +2,9 @@ function showOrHideVideo() {
     var x = document.getElementById("textEditVid");
     if (x.style.display === "none") {
       x.style.display = "inline-block";
-      document.getElementById("vidLink1").textContent= "Hide Video";
+      document.getElementById("vidLink1").textContent = "Hide Presentation Video";
     } else {
       x.style.display = "none";
-      document.getElementById("vidLink1").textContent = "Show Video";
+      document.getElementById("vidLink1").textContent = "Show Presentation Video";
     }
 }
